bbdbdd3acc6df1f7a4d2778ebcbc63610d15da5a
gaohangcodes <150210845+gaohangcodes@users.noreply.github.com>
2025-12-10 21:51:29 +0800

    Add files via upload

---
+++ codes on 100-Driver/scripts/train.sh
@@ -0,0 +1,42 @@
+GPU_ID=0
+cd ..
+
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-day-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-day-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-day-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-day-cam4
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-night-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-night-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-night-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_ca_14_distilledfromCA -dataset pic-night-cam4
+
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-day-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-day-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-day-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-day-cam4
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-night-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-night-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-night-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net can -dataset pic-night-cam4
+
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-day-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-day-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-day-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-day-cam4
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-night-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-night-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-night-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_teacher.py -net mobilenet_v2 -dataset pic-night-cam4
+
+
+
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-night-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-day-cam1
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-night-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-day-cam2
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-night-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-day-cam3
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-night-cam4
+#CUDA_VISIBLE_DEVICES=$GPU_ID python3 train_ca4sh.py -net my_11_withoutca -dataset pic-day-cam4
+
+
